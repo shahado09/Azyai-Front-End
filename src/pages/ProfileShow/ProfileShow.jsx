e3e70a2304b4f86ec3792879c554fd5d5dca528a
hiba-GA <hiba.formal@gmail.com>
2026-01-08 14:49:18 +0300

    checked

--- src/pages/ProfileShow/ProfileShow.jsx
+++ src/pages/ProfileShow/ProfileShow.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import * as profileService from "../../services/profile";
 
+
 const ProfileShow = () => {
   const { id } = useParams();
   const navigate = useNavigate();
